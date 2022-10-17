34df9191911debe7163eb3ab606dabc8bb103a47
Lonskoy <lonskoy0304@gmail.com>
2022-10-18 01:40:43 +0300

    edit4

--- task.js
+++ task.js
@@ -1,41 +1,32 @@
 // Задание 1
-const array1 = [10, 15, 20];
 
 function getArrayParams(arr) {
-  const min = arr.reduce((x) => Math.min(x));
-  const max = arr.reduce((x, y) => Math.max(x, y)); 
-  const avg = arr.reduce((acc, item, index, arr) => {
-      acc += item;
-      if (index === arr.length -1) {
-          return acc / arr.length;
-      }
-      return acc;
-  }, 0)
-
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const avg1 = arr.reduce((acc, item) => acc += item);
+  const avg = Number((avg1 / array.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 
 // Задание 2
 
-var sum = 0;
-
 function worker(arr) {
-  arr.forEach(function(value, index) {
-    Array.isArray(value) ? worker(value) : sum += value; 
-  });
-
+  let sum;
+  sum = arr.reduce((acc, item) => acc += item, 0);
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+function makeWork(arr, worker) {
+  let max = 0;
+  let temp = 0;
+  for (let i = 0; i < arr.length; i++) {
+    temp += worker(arr[i]);
+  }
+  if (temp > max) {max = temp}
   return max;
 }
+
 
 // Задание 3
 function worker2(arr) {
